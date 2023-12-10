6714853c7051dd55161ca7dbe1292f5a47a3a217
slimonslimon <slimonslimon@users.noreply.github.com>
2023-12-10 21:07:13 +0000

    Switch workflows to AIRDOS04, add create prerelease and release, extended build, extended build, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "260a521e6b3ab2e76ca63ed51661264d77b7cced,User";
-#define GHRELEASE 0
+String githash = "a5558820423b33fbac20c32c4677790c4759b100,User";
+#define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
