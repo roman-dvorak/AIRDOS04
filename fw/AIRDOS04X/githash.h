e89cf5ad05f52cfe0b9a94b881353dcc35738581
kakl <kakl@astro.cz>
2023-12-11 23:34:09 +0100

    FW for battery less version

---
+++ fw/AIRDOS04X/githash.h
@@ -0,0 +1,5 @@
+// This file is overwritten by github actions, do not update it manually
+String githash = "5273d0b12b78711e73af5b7ef59c05a43501fe2d,User";
+#define GHRELEASE 1
+#define GHBUILD 0
+#define GHBUILDTYPE User
