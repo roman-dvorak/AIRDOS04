0d6958badfef23cdeea0916732c115370a50a8fe
kakl <kakl@users.noreply.github.com>
2023-12-11 01:36:42 +0000

    device type changed to AIRDOS04A, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "a7a9d0eb9695f76b9cf8bf58b971ec95546936dd,User";
+String githash = "d12e6ac067fddedb07981c0f60c170de42849ba1,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
