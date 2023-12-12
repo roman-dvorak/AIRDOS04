cb9f16ed0fad7a9be1614bf05725bddc34cc49f0
kakl <kakl@users.noreply.github.com>
2023-12-12 13:11:24 +0000

    Merge branch 'AIRDOS04A' of github.com:UniversalScientificTechnologies/AIRDOS04 into AIRDOS04A, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "e00e6aa37d3ab9dfc9e47fc89ffaecdd1112bc5e,User";
+String githash = "c0b66cbeb737a349eec8946da0c87d41edd9a1d5,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
