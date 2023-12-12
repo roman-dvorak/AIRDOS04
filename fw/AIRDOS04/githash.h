25083f98a536eba89caf76610c177d61b218224b
roman-dvorak <roman-dvorak@users.noreply.github.com>
2023-12-12 18:24:52 +0000

    Update build_fw.yml, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "c0b66cbeb737a349eec8946da0c87d41edd9a1d5,User";
+String githash = "7e8122713cd22f04d5865026d0931dfad7ce4cbf,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
