ddec043b9ae64cb25e31cf5ef271af294f39df76
kakl <kakl@users.noreply.github.com>
2023-12-13 00:59:57 +0000

    reboot after power off for reinitiation charger, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "1905b9179096e859818e6010ad4eb126c2f0222c,User";
+String githash = "59b584c44dad017b3dd25c7d26393b848691673a,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
