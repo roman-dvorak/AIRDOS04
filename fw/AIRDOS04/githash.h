b15eea41dca5875bd6da7ab33085b881295d71fe
kaklik <kaklik@users.noreply.github.com>
2024-01-19 20:51:13 +0000

    Update .gitignore, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "fa714456a1656f776ce252b96f4c1c9a42ef939c,User";
+String githash = "5449b72a3cc66eb75bed78b13c80cfb0cfd06129,User";
 #define GHRELEASE 2
 #define GHBUILD 0
 #define GHBUILDTYPE User
