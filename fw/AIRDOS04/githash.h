2b2aabf783d165569a0e62d41e383d912d8d766f
kaklik <kaklik@users.noreply.github.com>
2024-03-29 15:24:11 +0000

    Revert "Attempt to even decrease a click treshold., extended build", extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "f6017565c858be2d7156b53efa12d1f940afef9c,User";
+String githash = "7a82b91233b084547bdd6ef19ad24afb65fa8aaf,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
