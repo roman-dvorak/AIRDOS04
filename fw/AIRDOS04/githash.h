c43094cd1c2f8fcc7258cabb6858b1b389897806
kaklik <kaklik@users.noreply.github.com>
2024-04-04 23:05:36 +0000

    Select stop-watch RTC mode only., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "1a6eea8d910899d8ead18a6a425e718e6bc96cfa,User";
+String githash = "14139fab47d0b94285b8d6f9b9052040f92f6400,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
