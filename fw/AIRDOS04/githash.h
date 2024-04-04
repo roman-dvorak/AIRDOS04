5239404866f37967b08df1c340d5b9b8b01b9185
kaklik <kaklik@users.noreply.github.com>
2024-04-04 23:48:04 +0000

    Explicitly set INTA pin mode., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "14139fab47d0b94285b8d6f9b9052040f92f6400,User";
+String githash = "d7f1f70c9db0cc6fd2f73e48e2de73dee9a51543,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
