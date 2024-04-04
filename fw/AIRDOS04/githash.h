fddb1520e797f0c5860b724d8df4ebe944074ab2
kaklik <kaklik@users.noreply.github.com>
2024-04-04 21:45:06 +0000

    Completely disable RTC initialization  #28, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "496682fe6669866244899569bdfbdb12fc6f25b5,User";
+String githash = "2ccd34c25b37b85b5eff95141dc7487dab5d3847,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
