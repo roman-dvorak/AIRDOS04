91c5a2f42c2cafc08e0113ef74503cda14ad0459
kaklik <kaklik@users.noreply.github.com>
2024-04-08 09:43:15 +0000

    Merge remote-tracking branch 'origin/dev' into AIRDOS04A, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "86ff8fc34dbe7b847a390505753f305e051dc58e,User";
+String githash = "024e8d29042857afa79d6701768c623f252a1587,User";
 #define GHRELEASE 0
 #define GHBUILD 0
 #define GHBUILDTYPE User
