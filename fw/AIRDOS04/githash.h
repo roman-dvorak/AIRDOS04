b62801c8c68fa6176555ebce2e2c3a9e739b34f1
kaklik <kaklik@users.noreply.github.com>
2024-06-15 10:08:25 +0000

    Change enviromental (pressure) data output interval to same length as integration interval e.g. 10 seconds., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "059105d0adb1b8706a2c93dfa1840c3a36f70ef4,User";
+String githash = "9dcf14bd62a0a1d7012306afa0277c41cd253500,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
