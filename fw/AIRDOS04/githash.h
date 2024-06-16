289360ab2fbc70dde1a74c9d164e81a45ee99799
kaklik <kaklik@users.noreply.github.com>
2024-06-16 10:34:00 +0000

    Merge branch 'FyzTyd2024' of github.com:UniversalScientificTechnologies/AIRDOS04 into FyzTyd2024, extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "15e7888167f7143e72ff2d918425b27bc1d8a111,User";
+String githash = "226617ab07dca11d50ad217e49971fbedb55f4bf,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
