00a183a971c6145c14fb9cebf7e7e5c6b1ff504a
kaklik <kaklik@users.noreply.github.com>
2024-06-16 08:30:10 +0000

    Reduce interval of Hits logging to maximally 10 minutes. Reduce array lenght of histogram to the CHANNELS constant. Remove RANGE constant, replacing it with CHANNELS., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "7d42ccb77e9a115bdd6ab983a42a4e89bbda3fe0,User";
+String githash = "c41002ce861fd430e51909f334f2ecf3695395a6,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
