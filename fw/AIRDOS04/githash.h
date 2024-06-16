3ab90ad26f4ee407826493a5d9a397c574a482a8
kaklik <kaklik@users.noreply.github.com>
2024-06-16 11:23:09 +0000

    Replacing noise variable channel definition with a constant NOISE., extended build

--- fw/AIRDOS04/githash.h
+++ fw/AIRDOS04/githash.h
@@ -1,5 +1,5 @@
 // This file is overwritten by github actions, do not update it manually
-String githash = "08c6ff435d26b416ab38b4ddaf198335270fa987,User";
+String githash = "2544b0dc5759b1c645eac69378228bc07a62a85f,User";
 #define GHRELEASE 1
 #define GHBUILD 0
 #define GHBUILDTYPE User
